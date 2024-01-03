7e41c7ecf10511371854d4e566c1b732ea30180f
Were Samson Bruno <werefast2000@gmail.com>
2024-01-03 17:09:35 +0000

    installed material ui icons

--- components/utils/icon.util.jsx
+++ components/utils/icon.util.jsx
@@ -4,15 +4,12 @@ import { useEffect, useState } from 'react'
 // Font Awesome packages
 const { library, config } = require('@fortawesome/fontawesome-svg-core')
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { fat } from '@fortawesome/pro-thin-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // Load icons into
-library.add(fat, fal, fas, fad, far, fab)
+library.add(fas, far, fab)
 
 /**
  * Icon factory utility.
