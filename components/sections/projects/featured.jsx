208f6601a8f45d6f4aac228befb16a040b45c1c3
Were Samson Bruno <werefast2000@gmail.com>
2024-01-08 16:35:41 +0000

    rename class to className

--- components/sections/projects/featured.jsx
+++ components/sections/projects/featured.jsx
@@ -1,41 +1,40 @@
-import FeaturedProject from '../../blocks/projects/featured'
-
+import FeaturedProject from '../../blocks/projects/featured';
 
 // Section structure
-import Section 		from '../../structure/section';
-import Container 	from '../../structure/container';
-import Badges 		from '../../utils/badge.list.util'
-import Icon 		from '../../utils/icon.util'
-import SectionTitle from '../../blocks/section.title.block'
+import Section from '../../structure/section';
+import Container from '../../structure/container';
+import SectionTitle from '../../blocks/section.title.block';
 
-import css 			from '../../../styles/sections/projects/featured.module.scss'
-import content 		from '../../../content/projects/featured.json'
+import css from '../../../styles/sections/projects/featured.module.scss';
+import content from '../../../content/projects/featured.json';
 
 export default function FeaturedProjects() {
-
-	return (
-		<Section classProp={css.hasBg}>	
-			<Container spacing={'verticalXXXXLrg'}>
-				<SectionTitle
-					title="Featured Projects"
-					preTitle="Design & Frontend"
-					subTitle="I am Focused on the User Experience, and Beautiful Interfaces."
-				/> 				{
-				content.map( (data, index) => {
-					return (
-						<FeaturedProject content={data} index={index} key={index} />
-					)
-				})
-				}
-			</Container>
-			<div className={css.bgContainer}>
-				<span className={css.orbitalBg}>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroLeft} ${css.heroOrbital}`}></span></span>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroCenter}`}></span></span>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroRight} ${css.heroOrbital}`}></span></span>
-				</span>
-				<span className={css.afterGlowBg}></span>
-			</div>
-		</Section>
-	)
+  return (
+    <Section className={css.hasBg}>
+      <Container spacing={'verticalXXXXLrg'}>
+        <SectionTitle
+          title="Featured Projects"
+          preTitle="Design & Frontend"
+          subTitle="I am Focused on the User Experience, and Beautiful Interfaces."
+        />
+        {content.map((data, index) => (
+          <FeaturedProject content={data} index={index} key={index} />
+        ))}
+      </Container>
+      <div className={css.bgContainer}>
+        <span className={css.orbitalBg}>
+          <span className={`${css.bgSection}`}>
+            <span className={`${css.bgInner} ${css.heroLeft} ${css.heroOrbital}`}></span>
+          </span>
+          <span className={`${css.bgSection}`}>
+            <span className={`${css.bgInner} ${css.heroCenter}`}></span>
+          </span>
+          <span className={`${css.bgSection}`}>
+            <span className={`${css.bgInner} ${css.heroRight} ${css.heroOrbital}`}></span>
+          </span>
+        </span>
+        <span className={css.afterGlowBg}></span>
+      </div>
+    </Section>
+  );
 }
