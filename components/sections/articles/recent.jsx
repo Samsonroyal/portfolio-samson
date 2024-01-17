f67318088b9c11c43b5753db192311e8966786a7
Were Samson Bruno <werefast2000@gmail.com>
2024-01-17 23:12:53 +0000

    refactored recent.jsx file

--- components/sections/articles/recent.jsx
+++ components/sections/articles/recent.jsx
@@ -20,50 +20,40 @@ export default function Recent({ mediumArticles }) {
           subTitle="A journey to document and share my personal experiences as a startup founder and software engineer."
         />
         <section className={css.projects}>
-          {articles.map(
-            (
-              { title, pubDate, link, author, thumbnail, categories },
-              index
-            ) => {
-              const date = new Date(pubDate).toDateString();
-              return (
-                <>
-                  <article key={index} className={css.project}>
-                    <span className={css.featuredImage}>
-                      <Image
-                        src={thumbnail}
-                        alt="Article thumbnail"
-                        layout="fill"
-                      />
+          {articles.map((article, index) => {
+            const date = new Date(article.pubDate).toDateString();
+            return (
+              <article key={article.id} className={css.project}> {/* Use article.id as the key */}
+                <span className={css.featuredImage}>
+                  <Image
+                    src={article.thumbnail}
+                    alt="Article thumbnail"
+                    layout="fill"
+                  />
+                </span>
+                <span className={css.header}>
+                  <a href={article.link} rel="noreferrer" target="_blank">
+                    {article.title} {" "}
+                    <Icon icon={["fad", "arrow-up-right-from-square"]} />
+                  </a>
+                </span>
+                <span className={css.descriptionContainer}></span>
+                <span className={css.details}>
+                  <p>By {article.author}</p>
+                  <p className={css.pushedAt}>{date}</p>
+                </span>
+                <span className={css.topicsContainer}>
+                  {article.categories.map((category, index) => (
+                    <span key={category} className={css.topics}> {/* Use category as the key */}
+                      <Icon icon={["fab", "medium"]} /> {category}
                     </span>
-                    <span className={css.header}>
-                      <a href={link} rel="noreferrer" target="_blank">
-                        {title}{" "}
-                        <Icon icon={["fad", "arrow-up-right-from-square"]} />
-                      </a>
-                    </span>
-                    <span className={css.descriptionContainer}></span>
-                    <span className={css.details}>
-                      <p>By {author}</p>
-                      <p className={css.pushedAt}>{date}</p>
-                    </span>
-                    <span className={css.topicsContainer}>
-                      {categories.map((e, index) => {
-                        return (
-                          <span key={index} className={css.topics}>
-                            <Icon icon={["fab", "medium"]} /> {e}
-                          </span>
-                        );
-                      })}
-                    </span>
-                  </article>
-                </>
-              );
-            }
-          )}
+                  ))}
+                </span>
+              </article>
+            );
+          })}
         </section>
       </Container>
     </Section>
   );
 }
-
